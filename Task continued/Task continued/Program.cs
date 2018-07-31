46dc367a06f3c5e5839abb78395b3a9a0ff12eac
Caitlin Pretorius <30002731@student.toiohomai.ac.nz>
2018-08-01 09:42:37 +1200

    exercise 2

---
+++ Task continued/Task continued/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_continued
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+
+            Console.WriteLine("Enter a number");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter a second number");
+            num2 = int.Parse(Console.ReadLine());
+
+            Math numbers = new Math();
+            Console.WriteLine("{0} x {1} = {2}", num1, num2, numbers.MultiplyNumbers(num1, num2));
+            Console.ReadLine();
+        }
+    }
+    class Math
+    {
+        public int MultiplyNumbers(int _num1, int _num2)
+        {
+            return _num1 * _num2;
+        }
+    }
+}
--- methods/methods/Program.cs
+++ methods/methods/Program.cs
@@ -10,6 +10,12 @@ namespace methods
     {
         static void Main(string[] args)
         {
+            MultiplyNums();
+            TaskB.taskB();
+            Math.taskC();
+        }
+        static void MultiplyNums()
+        {
             int num1 = 0;
             int num2 = 0;
             int total = 0;
@@ -21,7 +27,47 @@ namespace methods
             num2 = int.Parse(Console.ReadLine());
 
             total = num1 * num2;
-            Console.WriteLine(total);
+            Console.WriteLine($"{num1} x {num2} = {total}");
+            Console.ReadLine();
+        }
+    }
+    public class TaskB
+    {
+        public static void taskB()
+        {
+            int num1 = 0;
+            int num2 = 0;
+
+            Console.WriteLine("Enter a number");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter a second number");
+            num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0} x {1} = {2}", num1, num2, MultiplyNumsAgain(num1, num2));
+            Console.ReadLine();
+        }
+        public static int MultiplyNumsAgain(int _num1, int _num2)
+        {
+            return _num1 * _num2;
+        }
+    }
+    class Math
+    {
+        public static void taskC()
+        {
+            int num1 = 0;
+            int num2 = 0;
+            int total = 0;
+
+            Console.WriteLine("Enter a number");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter a second number");
+            num2 = int.Parse(Console.ReadLine());
+
+            total = num1 * num2;
+            Console.WriteLine($"{num1} x {num2} = {total}");
             Console.ReadLine();
         }
     }
